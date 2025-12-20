4ba9e937ace00068d62ec529476f2d761bf9f7a2
Novikova Ulyana <novikova.ula.007@gmail.com>
2025-12-20 13:13:21 +0300

    interval for receiving new messages and request for sending a message

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -2,14 +2,14 @@ import MessageForm from "./MessageForm/MessageForm.tsx";
 import {Box, Container} from "@mui/material";
 import type {messageInterface} from "../../types";
 import {useEffect, useState} from "react";
-import dayjs from "dayjs";
 import Message from "./Message/Message.tsx";
 
 const URL = 'http://146.185.154.90:8000/messages'
 
 const Chat = () => {
-    const [messages, setMessages] = useState<messageInterface[] | []>([])
-    const [lastTime, setLastTime] = useState<string | null>(null)
+    const [messages, setMessages] = useState<messageInterface[] | []>([]);
+    const [lastTime, setLastTime] = useState<string | null>(null);
+
     const requestFetch = async (url: string, init = {}): Promise<messageInterface[]> => {
         const response = await fetch(url, init);
         if (!response.ok) {
@@ -29,12 +29,40 @@ const Chat = () => {
         setTimeout(scrollToBottom, 0);
     }
 
+
     useEffect(() => {
+        let lastDate: string | null = null;
+        let interval: number;
+
+        const getNewMessages = async () => {
+            if (!lastTime) {
+                return;
+            }
+
+            if (lastTime) {
+                try {
+                    const URLTime = `${URL}?datetime=${lastTime}`;
+                    const newMessages = await requestFetch(URLTime);
+                    newMessages.reverse();
+                    if (newMessages.length > 0) {
+                        const lastDate = newMessages[0].datetime;
+                        setMessages([...newMessages, ...messages]);
+                        setLastTime(lastDate);
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+
+            }
+        }
+
+
         const getAllMessages = async () => {
             try {
                 const allMessages = await requestFetch(URL);
+                allMessages.reverse();
                 if (allMessages.length > 0) {
-                    const lastDate = dayjs(allMessages.reverse()[0].datetime).format('DD.MM.YYYY (dddd) - HH:mm');
+                    lastDate = allMessages[0].datetime;
                     setMessages(allMessages);
                     setLastTime(lastDate);
                 }
@@ -43,8 +71,18 @@ const Chat = () => {
             }
         }
 
-        void getAllMessages();
-    }, [])
+        if (!lastTime) {
+            void getAllMessages();
+        } else {
+            interval = setInterval(getNewMessages, 2000);
+        }
+
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+        }
+    }, [lastTime, messages])
 
     return (
         <Container fixed>
@@ -57,7 +95,7 @@ const Chat = () => {
                     borderRadius: 2,
                     marginBottom: '10px',
                     display: 'flex',
-                    flexDirection: 'column',
+                    flexDirection: 'column-reverse',
                     overflowY: 'scroll',
                 }}
                 id='chat'
--- src/containers/Chat/MessageForm/MessageForm.tsx
+++ src/containers/Chat/MessageForm/MessageForm.tsx
@@ -4,32 +4,59 @@ import {Button} from "@mui/material";
 import {useState} from "react";
 import * as React from "react";
 
+interface MessageForm {
+    text: string;
+    author: string;
+}
+
 const MessageForm = () => {
-    const [message, setMessage] = useState<string>('')
+    const [message, setMessage] = useState<MessageForm>({text: '', author: ''})
 
     const changeMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setMessage(e.target.value);
+        setMessage(prev => ({...prev, [e.target.name]: e.target.value}))
     };
 
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        if (!message.trim().length) {
+        if (!message.author.trim().length || !message.text.trim().length) {
             alert('You can not send empty field')
+        } else {
+            const url = 'http://146.185.154.90:8000/messages';
+            const data = new URLSearchParams();
+            data.set('message', message.text);
+            data.set('author', message.author);
+
+            await fetch(url, {
+                method: 'post',
+                body: data,
+            });
         }
-        setMessage('')
+
+        setMessage(prev => ({...prev, text: ''}))
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <TextField
+                    id="author"
+                    name="author"
+                    multiline
+                    placeholder="Enter your name"
+                    sx={{width: '250px',
+                        display: 'block',
+                    }}
+                    value={message.author}
+                    onChange={changeMessage}
+                />
+                <TextField
                     id="text"
-                    label="Your message"
+                    name="text"
                     multiline
                     rows={4}
                     placeholder="Enter your message here"
                     sx={{width: '500px'}}
-                    value={message}
+                    value={message.text}
                     onChange={changeMessage}
                 />
                 <Button variant="contained"
